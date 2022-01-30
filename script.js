ca43dd0270ca5c5d64fac475e4690452a1f0e2f4
unknown <rafahmp123@gmail.com>
2022-01-30 20:21:20 -0300

    Adicionando animação ao scroll

--- script.js
+++ script.js
@@ -58,3 +58,26 @@ function initScrollSuave() {
   });
 }
 initScrollSuave();
+
+function initScrollAnimation() {
+  const sections = document.querySelectorAll(".js-scroll");
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowMetade < 0;
+
+        if (isSectionVisible) {
+          section.classList.add("ativo");
+        } else {
+          section.classList.remove("ativo");
+        }
+      });
+    }
+  }
+  animaScroll();
+
+  window.addEventListener("scroll", animaScroll);
+}
+initScrollAnimation();
