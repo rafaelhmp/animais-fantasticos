76989bc7c11b9220d843918ae2b7073ac96038b0
unknown <rafahmp123@gmail.com>
2022-03-07 18:45:20 -0300

    Corrigindo script horario funcionamento

--- js/modules/funcionamento.js
+++ js/modules/funcionamento.js
@@ -9,7 +9,7 @@ export default function initFuncionamento() {
 
   const semanaAberto = diasSemana.indexOf(diaAgora) !== -1;
   const horarioAberto =
-    horarioAgora >= horarioSemana[0] || horarioAgora < horarioSemana[1];
+    horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1];
 
   if (semanaAberto && horarioAberto) {
     funcionamento.classList.add("aberto");
